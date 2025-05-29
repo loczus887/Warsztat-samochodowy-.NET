4c7b30b3ca60e1b76db6341a9c47ab44d5d3c7ae
Alicja Kowalska <alakowalska2000@wp.pl>
2025-05-29 22:39:00 +0200

    Add Vehicles controller and image upload

---
+++ WorkshopManager/Controllers/VehiclesController.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WorkshopManager.DTOs;
+using WorkshopManager.Mappers;
+using WorkshopManager.Models;
+using WorkshopManager.Services.Interfaces;
+
+namespace WorkshopManager.Controllers;
+
+[Authorize(Roles = "Admin,Receptionist")]
+public class VehiclesController : Controller
+{
+    private readonly IVehicleService _vehicleService;
+    private readonly ICustomerService _customerService;
+    private readonly VehicleMapper _mapper;
+
+    public VehiclesController(
+        IVehicleService vehicleService,
+        ICustomerService customerService,
+        VehicleMapper mapper)
+    {
+        _vehicleService = vehicleService;
+        _customerService = customerService;
+        _mapper = mapper;
+    }
+
+    // GET: Vehicles
+    public async Task<IActionResult> Index()
+    {
+        var vehicles = await _vehicleService.GetAllVehiclesAsync();
+        var vehicleDtos = _mapper.VehiclesToDto(vehicles);
+        return View(vehicleDtos);
+    }
+
+    // GET: Vehicles/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var vehicle = await _vehicleService.GetVehicleByIdAsync(id.Value);
+        if (vehicle == null)
+        {
+            return NotFound();
+        }
+
+        var vehicleDto = _mapper.VehicleToDto(vehicle);
+        return View(vehicleDto);
+    }
+
+    // GET: Vehicles/Create
+    public async Task<IActionResult> Create(int? customerId)
+    {
+        await PopulateCustomersDropDownList(customerId);
+        return View();
+    }
+
+    // POST: Vehicles/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(VehicleDto vehicleDto)
+    {
+        if (ModelState.IsValid)
+        {
+            var vehicle = _mapper.DtoToVehicle(vehicleDto);
+
+            // Obsługa uploadu zdjęcia
+            if (vehicleDto.ImageFile != null)
+            {
+                vehicle.ImageUrl = await _vehicleService.SaveVehicleImageAsync(vehicleDto.ImageFile);
+            }
+
+            await _vehicleService.CreateVehicleAsync(vehicle);
+            return RedirectToAction(nameof(Index));
+        }
+
+        await PopulateCustomersDropDownList(vehicleDto.CustomerId);
+        return View(vehicleDto);
+    }
+
+    // GET: Vehicles/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var vehicle = await _vehicleService.GetVehicleByIdAsync(id.Value);
+        if (vehicle == null)
+        {
+            return NotFound();
+        }
+
+        var vehicleDto = _mapper.VehicleToDto(vehicle);
+        await PopulateCustomersDropDownList(vehicle.CustomerId);
+
+        return View(vehicleDto);
+    }
+
+    // POST: Vehicles/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, VehicleDto vehicleDto)
+    {
+        if (id != vehicleDto.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                var vehicle = _mapper.DtoToVehicle(vehicleDto);
+
+                // Sprawdź, czy aktualizujemy zdjęcie
+                if (vehicleDto.ImageFile != null)
+                {
+                    vehicle.ImageUrl = await _vehicleService.SaveVehicleImageAsync(vehicleDto.ImageFile);
+                }
+                else
+                {
+                    // Zachowaj istniejące zdjęcie
+                    var existingVehicle = await _vehicleService.GetVehicleByIdAsync(id);
+                    if (existingVehicle != null)
+                    {
+                        vehicle.ImageUrl = existingVehicle.ImageUrl;
+                    }
+                }
+
+                await _vehicleService.UpdateVehicleAsync(vehicle);
+            }
+            catch (Exception)
+            {
+                if (!await VehicleExists(vehicleDto.Id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        await PopulateCustomersDropDownList(vehicleDto.CustomerId);
+        return View(vehicleDto);
+    }
+
+    // GET: Vehicles/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var vehicle = await _vehicleService.GetVehicleByIdAsync(id.Value);
+        if (vehicle == null)
+        {
+            return NotFound();
+        }
+
+        var vehicleDto = _mapper.VehicleToDto(vehicle);
+        return View(vehicleDto);
+    }
+
+    // POST: Vehicles/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _vehicleService.DeleteVehicleAsync(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+    private async Task<bool> VehicleExists(int id)
+    {
+        var vehicle = await _vehicleService.GetVehicleByIdAsync(id);
+        return vehicle != null;
+    }
+
+    private async Task PopulateCustomersDropDownList(int? selectedCustomerId = null)
+    {
+        var customers = await _customerService.GetAllCustomersAsync();
+        ViewBag.CustomerId = new SelectList(
+            customers.Select(c => new
+            {
+                Id = c.Id,
+                FullName = $"{c.FirstName} {c.LastName}"
+            }),
+            "Id",
+            "FullName",
+            selectedCustomerId);
+    }
+}
--- WorkshopManager/obj/Debug/net8.0/WorkshopManager.AssemblyInfo.cs
+++ WorkshopManager/obj/Debug/net8.0/WorkshopManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkshopManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c04acbb43d492edcd1dc3a2b5bee8335e08966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+104221cac03dd08ab010259c40dbd684d5f7c51b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkshopManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkshopManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
